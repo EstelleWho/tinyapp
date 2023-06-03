0511333bd85c683f54a8931231aee5b759bfca9f
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-06-03 18:33:02 -0400

    add longURL

--- express_server.js
+++ express_server.js
@@ -24,3 +24,15 @@ app.listen(PORT, () => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// to keep templates and urls 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// second route and template
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
